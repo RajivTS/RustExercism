2ec8e2ecb8f1dbc8cc6bfea1df369754060f7744
Rajiv Sharma <rajshar.email@gmail.com>
2022-12-10 13:48:16 +0530

    Exercism Poker

--- poker/src/lib.rs
+++ poker/src/lib.rs
@@ -30,6 +30,7 @@ enum Number {
     Seven,
     Eight,
     Nine,
+    Ten,
     Jack,
     Queen,
     King,
@@ -47,6 +48,7 @@ impl From<char> for Number {
             '7' => Self::Seven,
             '8' => Self::Eight,
             '9' => Self::Nine,
+            '1' => Self::Ten,
             'J' => Self::Jack,
             'Q' => Self::Queen,
             'K' => Self::King,
@@ -56,22 +58,17 @@ impl From<char> for Number {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 struct Card(Suit, Number);
 
 impl Card {
     fn new(input: &str) -> Self {
         let mut chars = input.chars();
-        match (chars.next(), chars.next()) {
-            (Some(number), Some(suit)) => Self(suit.into(), number.into()),
+        match (chars.next(), chars.next(), chars.next()) {
+            (Some(number), Some(suit), None) => Self(suit.into(), number.into()),
+            (Some(number), Some(_), Some(suit)) => Self(suit.into(), number.into()),
             _ => panic!("Incorrect card format {}", input),
         }
-    }
-}
-
-impl PartialEq for Card {
-    fn eq(&self, other: &Self) -> bool {
-        self.0 == other.0 && self.1 == other.1
     }
 }
 
@@ -79,21 +76,25 @@ impl Sub<&Card> for &Card {
     type Output = i32;
 
     fn sub(self, rhs: &Card) -> Self::Output {
-        let diff = self.1 as i32 - rhs.1 as i32;
-        diff.abs()
+        match (self.1, rhs.1) {
+            (Number::Two, Number::Ace) => 1,
+            (left, right) => (left as i32 - right as i32).abs(),
+        }
     }
 }
 
-#[derive(PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 enum Category {
     HighCard,
     OnePair,
     TwoPair,
     ThreeOfAKind,
+    StraightFiveHigh,
     Straight,
     Flush,
     FullHouse,
     FourOfAKind,
+    StraightFlushFiveHigh,
     StraightFlush,
 }
 
@@ -103,13 +104,13 @@ impl From<&[Card]> for Category {
         let suit_frequency = cards_suit_freq(cards);
         match cards {
             [first, second, third, fourth, fifth] => {
-                if first - second == 1
-                    && second - third == 1
-                    && third - fourth == 1
-                    && fourth - fifth == 1
+                if is_consecutive_diff_one(first, second, third, fourth, fifth)
                     && suit_frequency.len() == 1
                 {
-                    Self::StraightFlush
+                    match is_five_high(first, second, third, fourth, fifth) {
+                        true => Self::StraightFlushFiveHigh,
+                        false => Self::StraightFlush,
+                    }
                 } else if number_freq.len() == 2 {
                     if number_freq.iter().any(|&(_, s)| s == 4) {
                         Self::FourOfAKind
@@ -118,12 +119,11 @@ impl From<&[Card]> for Category {
                     }
                 } else if suit_frequency.len() == 1 {
                     Self::Flush
-                } else if first - second == 1
-                    && second - third == 1
-                    && third - fourth == 1
-                    && fourth - fifth == 1
-                {
-                    Self::Straight
+                } else if is_consecutive_diff_one(first, second, third, fourth, fifth) {
+                    match is_five_high(first, second, third, fourth, fifth) {
+                        true => Self::StraightFiveHigh,
+                        false => Self::Straight,
+                    }
                 } else if number_freq.iter().any(|&(_, s)| s == 3) {
                     Self::ThreeOfAKind
                 } else {
@@ -139,7 +139,7 @@ impl From<&[Card]> for Category {
     }
 }
 
-#[derive(PartialEq)]
+#[derive(Debug, PartialEq)]
 struct Hand<'a> {
     original_hand: &'a str,
     cards: [Card; 5],
@@ -170,87 +170,23 @@ impl PartialOrd for Hand<'_> {
             cards_number_freq(&self.cards),
             cards_number_freq(&other.cards),
         );
-        let suit_frequency = cards_suit_freq(&self.cards);
         let ord = match self.category.cmp(&other.category) {
             Ordering::Equal => match self.category {
-                Category::StraightFlush | Category::Straight => self.cards[0].1.cmp(&other.cards[0].1),
-                Category::FourOfAKind => {
-                    let self_quad_num = number_with_freq(&self_number_freq, 4);
-                    let self_single_num = number_with_freq(&self_number_freq, 1);
-                    let other_quad_num = number_with_freq(&other_number_freq, 4);
-                    let other_single_num = number_with_freq(&other_number_freq, 1);
-                    self_quad_num
-                        .0
-                        .cmp(&other_quad_num.0)
-                        .then(self_single_num.0.cmp(&other_single_num.0))
+                Category::StraightFlush | Category::Straight | Category::StraightFlushFiveHigh | Category::StraightFiveHigh => {
+                    self.cards[0].1.cmp(&other.cards[0].1)
                 }
-                Category::FullHouse => {
-                    let self_tri_num = number_with_freq(&self_number_freq, 3);
-                    let self_twice_num = number_with_freq(&self_number_freq, 2);
-                    let other_tri_num = number_with_freq(&other_number_freq, 3);
-                    let other_twice_num = number_with_freq(&other_number_freq, 2);
-                    self_tri_num
-                        .0
-                        .cmp(&other_tri_num.0)
-                        .then(self_twice_num.0.cmp(&other_twice_num.0))
+                Category::FourOfAKind | Category::FullHouse => {
+                    compare_cards_in_order(&self_number_freq, &other_number_freq, 2)
                 }
-                Category::Flush => self.cards[0]
-                    .1
-                    .cmp(&other.cards[0].1)
-                    .then(self.cards[1].1.cmp(&other.cards[1].1))
-                    .then(self.cards[2].1.cmp(&other.cards[2].1))
-                    .then(self.cards[3].1.cmp(&other.cards[3].1))
-                    .then(self.cards[4].1.cmp(&other.cards[4].1)),
                 Category::ThreeOfAKind | Category::TwoPair => {
-                    self_number_freq
-                        .get(0)
-                        .unwrap()
-                        .0
-                        .cmp(&other_number_freq.get(0).unwrap().0)
-                        .then(
-                            self_number_freq
-                                .get(1)
-                                .unwrap()
-                                .0
-                                .cmp(&other_number_freq.get(1).unwrap().0),
-                        )
-                        .then(
-                            self_number_freq
-                                .get(2)
-                                .unwrap()
-                                .0
-                                .cmp(&other_number_freq.get(2).unwrap().0),
-                        )
+                    compare_cards_in_order(&self_number_freq, &other_number_freq, 3)
                 }
                 Category::OnePair => {
-                    self_number_freq
-                        .get(0)
-                        .unwrap()
-                        .0
-                        .cmp(&other_number_freq.get(0).unwrap().0)
-                        .then(
-                            self_number_freq
-                                .get(1)
-                                .unwrap()
-                                .0
-                                .cmp(&other_number_freq.get(1).unwrap().0),
-                        )
-                        .then(
-                            self_number_freq
-                                .get(2)
-                                .unwrap()
-                                .0
-                                .cmp(&other_number_freq.get(2).unwrap().0),
-                        )
-                        .then(
-                            self_number_freq
-                                .get(3)
-                                .unwrap()
-                                .0
-                                .cmp(&other_number_freq.get(3).unwrap().0),
-                        )                      
+                    compare_cards_in_order(&self_number_freq, &other_number_freq, 4)
                 }
-                _ => Ordering::Equal,
+                Category::Flush | Category::HighCard => {
+                    compare_cards_in_order(&self_number_freq, &other_number_freq, 5)
+                }
             },
             ord => ord,
         };
@@ -258,30 +194,79 @@ impl PartialOrd for Hand<'_> {
     }
 }
 
+fn is_consecutive_diff_one(a: &Card, b: &Card, c: &Card, d: &Card, e: &Card) -> bool {
+    match ((a - b), (b - c), (c - d), (d - e)) {
+        (1, 1, 1, 1) | (9, 1, 1, 1) => true,
+        _ => false,
+    }
+}
+
+fn is_five_high(a: &Card, b: &Card, c: &Card, d: &Card, e: &Card) -> bool {
+    match ((a - b), (b - c), (c - d), (d - e)) {
+        (9, 1, 1, 1) => true,
+        _ => false,
+    }
+}
+
+fn compare_cards_in_order(
+    self_cards: &Vec<(Number, usize)>,
+    other_cards: &Vec<(Number, usize)>,
+    len: usize,
+) -> Ordering {
+    let mut ord = self_cards
+        .get(0)
+        .unwrap()
+        .0
+        .cmp(&other_cards.get(0).unwrap().0);
+    for idx in 1..len {
+        ord = ord.then(
+            self_cards
+                .get(idx)
+                .unwrap()
+                .0
+                .cmp(&other_cards.get(idx).unwrap().0),
+        );
+    }
+    ord
+}
+
 fn cards_number_freq(cards: &[Card]) -> Vec<(Number, usize)> {
     let mut cards_vec = cards
         .iter()
         .map(|c| (c.1, cards.iter().filter(|ca| ca.1 == c.1).count()))
+        .collect::<std::collections::HashSet<_>>()
+        .into_iter()
         .collect::<Vec<_>>();
     cards_vec.sort_by(|a, b| a.1.cmp(&b.1).then(a.0.cmp(&b.0)).reverse());
     cards_vec
-}
-
-fn number_with_freq(cards: &Vec<(Number, usize)>, freq: usize) -> &(Number, usize) {
-    cards.iter().filter(|n| n.1 == freq).next().unwrap()
 }
 
 fn cards_suit_freq(cards: &[Card]) -> Vec<(Suit, usize)> {
     cards
         .iter()
         .map(|c| (c.0, cards.iter().filter(|ca| ca.0 == c.0).count()))
+        .collect::<std::collections::HashSet<_>>()
+        .into_iter()
         .collect()
 }
+
 /// Given a list of poker hands, return a list of those hands which win.
 ///
 /// Note the type signature: this function should return _the same_ reference to
 /// the winning hand(s) as were passed in, not reconstructed strings which happen to be equal.
 pub fn winning_hands<'a>(hands: &[&'a str]) -> Vec<&'a str> {
-    let parsed_hands = hands.iter().map(|&h| Hand::new(h)).collect::<Vec<_>>();
-    unimplemented!("Out of {:?}, which hand wins?", hands)
+    let mut parsed_hands = hands.iter().map(|&h| Hand::new(h)).collect::<Vec<_>>();
+    parsed_hands.sort_by(|a, b| a.partial_cmp(&b).unwrap_or(Ordering::Equal).reverse());
+    dbg!(&parsed_hands);
+    let mut result: Vec<&'a str> = Vec::new();
+    if let Some(compare) = parsed_hands.first() {
+        result.extend(parsed_hands.iter().filter_map(|h| {
+            if compare.partial_cmp(h).unwrap_or(Ordering::Equal) == Ordering::Equal {
+                Some(h.original_hand)
+            } else {
+                None
+            }
+        }))
+    }
+    result
 }
