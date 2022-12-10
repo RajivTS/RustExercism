62a9604c70a9a0f9ead246f4fe1afb9d5a9af5c4
Rajiv Sharma <rajshar.email@gmail.com>
2022-12-10 18:46:53 +0530

    Exercism Low Powered Embedded Game

---
+++ low-power-embedded-game/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn divmod(dividend: i16, divisor: i16) -> (i16, i16) {
+    (dividend / divisor, dividend % divisor)
+}
+
+pub fn evens<T>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
+    iter.enumerate().filter_map(|(idx, elem)| {
+        match idx % 2 {
+            0 => Some(elem),
+            _ => None
+        }
+    }).into_iter()
+}
+
+pub struct Position(pub i16, pub i16);
+impl Position {
+    pub fn manhattan(&self) -> i16 {
+        self.0.abs() + self.1.abs()
+    }
+}
